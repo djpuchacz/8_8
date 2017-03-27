cac44681829eaab13729d194a607689f18180ff3
Łukasz Woźniak <ukasz Woniak>
2017-03-27 20:22:00 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+	animal = 'Zielone słonie';
+	animalUpper = animal.toUpperCase();
+	textAfterChange = text.replace('Papugi', animalUpper);
+	half = textAfterChange.slice(0, 45);
+console.log(half);
+
+
+
+
+
+
+
+
+
+
