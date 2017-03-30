8385b85032e75fa90fd7d821029638c22b30330a
Łukasz Woźniak <ukasz Woniak>
2017-03-30 20:59:29 +0200

    indentation correct

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var	text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
 var	animal = 'Zielone słonie';
 var	animalUpper = animal.toUpperCase();
 var	textAfterChange = text.replace('Papugi', animalUpper);
